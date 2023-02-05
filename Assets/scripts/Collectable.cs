bb0eedb78bfd297ba6f93b02ebb913c6446d3b3b
Peter Förster <p2f2@gmx.de>
2023-02-05 01:55:29 +0100

    collect

---
+++ Assets/scripts/Collectable.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    float speedBoost = 10f;
+    GameObject player;
+    MouseMovement playerScript;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player = collision.gameObject;
+            playerScript = player.GetComponent<MouseMovement>();
+
+            if (playerScript)
+            {
+
+                StartCoroutine(TimerForDisable());
+
+            }
+        }
+    }
+
+
+
+    IEnumerator TimerForDisable()
+    {
+        playerScript.downfallValue -= speedBoost;
+        Destroy(gameObject);
+
+        yield return new WaitForSeconds(2);
+
+        playerScript.downfallValue += speedBoost;
+    }
+
+}
